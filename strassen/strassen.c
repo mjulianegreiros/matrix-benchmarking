0b07cef8d8e31051c962d90e09147cd0b22b6f94
mjulianegreiros <xuxuuzinhaa8@gmail.com>
2025-12-29 21:24:21 -0300

    strassen completo mas sem alocacao de memoria

--- strassen/strassen.c
+++ strassen/strassen.c
@@ -73,10 +73,10 @@ void strassen(int **a , int **b , int **c , int tam){
         separa(a , a4 , tam/2 , tam/2 , tam/2);
 
         // separando a matriz b
-        separa(b , b1 , 0 , 0 , tam);
-        separa(b , b2 , 0 , tam/2 , tam);
-        separa(b , b3 , tam/2 , 0 , tam);
-        separa(b , b4 , tam/2 , tam/2 , tam);
+        separa(b , b1 , 0 , 0 , tam/2);
+        separa(b , b2 , 0 , tam/2 , tam/2);
+        separa(b , b3 , tam/2 , 0 , tam/2);
+        separa(b , b4 , tam/2 , tam/2 , tam/2   );
 
         // usando as fórmulas do algoritmo
         int **s1 , **s2 , **s3 , **s4 , **s5;
@@ -87,7 +87,7 @@ void strassen(int **a , int **b , int **c , int tam){
         soma(a3 , a4 , s3 , tam/2);
         subtrai(b3 , b1 , s4 , tam/2);
         soma(a1 , a4 , s5 , tam/2);
-        soma(a1 , a2 , s6 , tam/2);
+        soma(b1 , b4 , s6 , tam/2);
         subtrai(a2, a4 , s7 , tam/2);
         soma(b3 , b4 , s8 , tam/2);
         subtrai(a1 , a3 , s9 , tam/2);
@@ -95,8 +95,38 @@ void strassen(int **a , int **b , int **c , int tam){
 
         // multiplicando as somas
         int **p1 , **p2 , **p3 , **p4 , **p5 , **p6 , **p7;
-        strassen(a1 , s1 , c , )
+        strassen(a1 , s1 , p1 , tam/2);
+        strassen(s2 , b4 , p2 , tam/2);
+        strassen(s3 , b1 , p3 , tam/2);
+        strassen(a4 , s4 , p4 , tam/2);
+        strassen(s5 , s6 , p5 , tam/2);
+        strassen(s7 , s8 , p6 , tam/2);
+        strassen(s9 , s10 , p7 , tam/2);
         
+        // remontando a matriz C
+        int **t1 , **t2; // matrizes auxiliares para o processo de remontagem
+        int **c1 , **c2 , **c3 , **c4; // matrizes a serem remontadas
+        // remontando C1
+        soma(p5 , p4 , t1 , tam/2);
+        subtrai(t1 , p2 , t2 , tam/2);
+        soma(t2 , p6 , c1 , tam/2);
+
+        // remontando C2
+        soma(p3 , p5 , c2 , tam/2);
+
+        // remontando C3
+        soma(p3 , p4 , c3 , tam/2);
+
+        // remontando C4
+        soma(p5 , p1 , t1 , tam/2);
+        subtrai(t1 , p3 , t2 , tam/2);
+        soma(t2 , p7 , c4 , tam/2);
+
+        // juntando tudo 
+        junta(c1 , c , 0 , 0 , tam/2);
+        junta(c2 , c , 0 , tam/2 , tam/2);
+        junta(c3 , c , tam/2 , 0 , tam/2);
+        junta(c4 , c , tam/2 , tam/2 , tam/2);
         
     }
 }
